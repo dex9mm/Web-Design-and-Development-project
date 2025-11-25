34bd0a7a9ea5e11053e7ec4235ac43e224d64f59
dex9mm <okellyderek82@gmail.com>
2025-11-25 11:07:29 +0000

    third commit wdd project

--- Assets & Research/HTML pages/scripts/nav.js
+++ Assets & Research/HTML pages/scripts/nav.js
@@ -1,7 +1,7 @@
 // all worked on navbar development
 // Tristan, Alex, Derek, Andrei
 var navbar = `
-		<nav class="navbar bg-dark navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+		<nav class="navbar navbar-expand-lg">
 			<div class="container-fluid">
 				<a class="navbar-brand"  href="index.html">Group G</a>
 				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -9,14 +9,14 @@ var navbar = `
 				</button>
 
 				<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-					<a class="nav-link active navBtn" aria-current="page" href="index.html">Home</a>
-					<a class="nav-link navBtn"  href="news.html">News</a>
-					<a class="nav-link navBtn"  href="uisceire.html">UisceIRE</a>
-					<a class="nav-link navBtn"  href="sources.html">Sources</a>
-					<a class="nav-link navBtn"  href="conservation.html">Conservation</a>
-					<a class="nav-link navBtn"  href="findus.html">Find Us</a>
-					<a class="nav-link navBtn"  href="contact.html">Contact Us</a>
-					<a class="nav-link navBtn"  href="info.html">Info</a>
+					<a class="nav-link navBtn" href="index.html">Home</a>
+					<a class="nav-link navBtn" href="news.html">News</a>
+					<a class="nav-link navBtn" href="uisceire.html">UisceIRE</a>
+					<a class="nav-link navBtn" href="sources.html">Sources</a>
+					<a class="nav-link navBtn" href="conservation.html">Conservation</a>
+					<a class="nav-link navBtn" href="findus.html">Find Us</a>
+					<a class="nav-link navBtn" href="contact.html">Contact Us</a>
+					<a class="nav-link navBtn" href="info.html">Info</a>
 				</div>
 			</div>
 		</nav>`;
