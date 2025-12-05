8d2d9b80e59546329ebce3847f706df918f898e5
dex9mm <okellyderek82@gmail.com>
2025-12-05 18:26:39 +0000

    commit 5.0

--- Assets & Research/HTML pages/scripts/tbo_suggestions.js
+++ Assets & Research/HTML pages/scripts/tbo_suggestions.js
@@ -1,17 +1,19 @@
 // Tristan's form validation
 
-
+//when validForm() is called , it will validate each constant
 function validForm() {
-	const names = document.getElementById("names").value;
+	const names = document.getElementById("names").value;              
 	const surname = document.getElementById("surname").value;
 	const email = document.getElementById("email").value;
 	const suggest = document.getElementById("suggest").value;
 	
+	// this is used for detect any errors
 	const name_Err = document.getElementById("names_error");
 	const surn_Err = document.getElementById("surname_error");
 	const email_Err = document.getElementById("email_error");
 	const sug_Err = document.getElementById("sug_error");
 	
+	//this checks that the field is not empty 
 	name_Err.textContent = "";
 	surn_Err.textContent = "";
 	email_Err.textContent = "";
