330b7b5e7843984541fc502d97014e970088fc69
dex9mm <okellyderek82@gmail.com>
2025-12-05 20:26:36 +0000

    commit 5.0

--- Assets & Research/HTML pages/scripts/tbo_suggestions.js
+++ Assets & Research/HTML pages/scripts/tbo_suggestions.js
@@ -13,7 +13,7 @@ function validForm() {
 	const email_Err = document.getElementById("email_error");
 	const sug_Err = document.getElementById("sug_error");
 	
-	//this checks that the field is not empty 
+	//this is used to make sure the field is not left empty
 	name_Err.textContent = "";
 	surn_Err.textContent = "";
 	email_Err.textContent = "";
@@ -21,26 +21,31 @@ function validForm() {
 
 	var is_form_valid = true;
 	
+	//length check for names box
 	if(names === "" || names.length < 2){
 		name_Err.textContent = "Please eneter your name correctly";
 		is_form_valid = false;
 	}
 	
+	//length check for surname box
 	if(surname === "" || surname.length < 2){
 		surn_Err.textContent = "Please eneter your surname correctly";
 		is_form_valid = false;
 	}
 	
-	if(email === "" || email.length < 2){
+	//length check for email box
+	if(email === "" || email.length < 5){
 		email_Err.textContent = "Please eneter a valid Email";
 		is_form_valid = false;
 	}
 	
-	if(suggest === "" || suggest.length < 2){
+	//length check for suggestion box
+	if(suggest === "" || suggest.length < 10){
 		sug_Err.textContent = "Please eneter a suggestion";
 		is_form_valid = false;
 	}
 	
+	//if the is_form_valid remains true , they alert is displayed
 	if(is_form_valid){
 		alert("Form submitted successfuly");
 		return true;
